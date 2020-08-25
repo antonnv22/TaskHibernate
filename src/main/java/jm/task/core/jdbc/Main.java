beff2741bc62f749df921af559832d711a331859
AntonNV <antonnv@yandex.ru>
2020-08-25 08:12:43 +0300

    Практическая задача 1.1.3. Выполнено

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        User[] users = { new User("Ivan","Ivanov", (byte) 10),
+                         new User("Petr","Petrov", (byte) 20),
+                         new User("Semen","Sidorov", (byte) 30),
+                         new User("Petr","Ivanov", (byte) 40) };
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        for (User user : users ) {
+            userService.saveUser(user.getName(), user.getLastName(), user.getAge());
+            System.out.printf("User с именем – %s добавлен в базу данных\n",user.getName());
+        }
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,34 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DB_HOSTNAME ="localhost";
+    private static final String DB_DBNAME ="userDB";
+    private static final String DB_USERNAME ="root";
+    private static final String DB_PASSWORD ="Moscowneversleep2020";
+    // Connect to MySQL
+    public static Connection getMySQLConnection() {
+        return getMySQLConnection(DB_HOSTNAME, DB_DBNAME, DB_USERNAME, DB_PASSWORD);
+    }
+
+    public static Connection getMySQLConnection(String hostName, String dbName,
+                                                String userName, String password) {
+        Connection connection = null;
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName+
+                "?verifyServerCertificate=false"+
+                "&useSSL=false"+
+                "&requireSSL=false"+
+                "&useLegacyDatetimeCode=false"+
+                "&amp"+
+                "&serverTimezone=UTC";
+        try {
+            connection = DriverManager.getConnection(connectionURL, userName, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
